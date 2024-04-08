c402613a47ed0e2c819202e0708b1f53c88d0706
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-08 17:37:51 +0200

    fix: comments in english

--- src/app/src/components/button/Button.js
+++ src/app/src/components/button/Button.js
@@ -1,13 +1,13 @@
-//Componente de react que coloca un boton
-//Parametros requeridos:
-// - debe solicitar el texto interno
-// - debe solicitar la accion que se realizara al pulsarse
+//Button Component
+//Parameters:
+// - asks for internal text
+// - asks action to do
 
 
 import React from 'react';
 
 function Button(props) {
-//Extrae el valor del texto y la accion de cuando se pulsa (onClick)
+//extracts the vaue of the text and the onClick action of the button
   const { text, onClick } = props;
 
   return (
@@ -20,20 +20,20 @@ export default Button;
 
 
 
-//1. DESDE LA APP DEBE HABER UNA FUNCION TIPO "HANDLE" QUE SE EJECUTE CON EL CLICK DEL BOTON
-//2. PONEMOS EL BOTON CON EL TEXTO QUE SEA Y LA FUNCION EVENTO QUE SE EJECUTARA CUANDO SE HAGA CLICK EN EL BOTON -> AL BOTON LE PASARA EL TEXTO Y LA ACCION DE CUANDO SE PULSE
-//EN LA APLICACION MAIN SERIA ALGO COMO:
+//1. Handle function does action
+//2. Button gives the text and the onClick action
+//APP EXAMPLE
 //import React from 'react';
 //import Button from './Button';
 //function App() {
 //  const handleClick = () => {
-//    console.log('¡El botón ha sido clicado!');
+//    console.log('Button Clicked!');
 //  };
 
 //  return (
 //    <div>
-//      <h1>Componente de Botón</h1>
-//      <Button texto="Haz clic aquí" onClick={handleClick} />
+//      <h1>Button Component</h1>
+//      <Button text="Haz clic aquí" onClick={handleClick} />
 //    </div>
 //  );
 //}
