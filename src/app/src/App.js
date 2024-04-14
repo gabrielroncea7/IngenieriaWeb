3fbc1e2380828cf0e90938ecf3ef495c3e86361e
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-14 13:54:08 +0200

    fix: added navigation to signin and signup

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+
+
 function App() {
   return (
     <>
       Wordle
     </>
+
+//ROUTES HERE
+  <Router>
+      <Switch>
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+    //First  page that shows when user enters site is sign up page to create an account
+        <Redirect from="/" to="/signup" />
+//MORE ROUTES HERE
+    
+      </Switch>
+    </Router>
   );
 }
 
