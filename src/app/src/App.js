d90931007433ba1bd65716acb49da2cbda8d40d8
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-15 17:02:48 +0200

    fix: added Game route

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
+import Game from './Game';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route index element={<Navigate to="/signup" replace/>}/>
           <Route exact path="/signup" element={<SignUp/>} />
-          <Route exact path="/signin" element={<SignIn/>} />    
+          <Route exact path="/signin" element={<SignIn/>} />
+          <Route exact path="/game" element={<Game/>} />
         </Routes>
       </Router>
     </>
