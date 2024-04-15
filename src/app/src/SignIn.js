0b093706b51210d4168e71816b35fd7243d09b0e
Yo <franjomor@gmail.com>
2024-04-15 13:49:24 +0200

    fix: sign in button

--- src/app/src/SignIn.js
+++ src/app/src/SignIn.js
@@ -54,7 +54,7 @@ const SignIn = () => {
 			{ Type: 'input', Name: 'username', Placeholder: 'Username', Value: username, OnChange: handleUsernameChange },
       			{ Type: 'password', Name: 'password', Placeholder: 'Password', Value: password, OnChange: handlePasswordChange }
     		],
-    		Button: { type: 'submit', text: 'Sign In' }
+    		button: { type: 'submit', text: 'Sign In' }
   	};
 	
   return (
