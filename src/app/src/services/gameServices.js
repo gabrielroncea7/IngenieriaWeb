5237d5e2e99f52d8b49211e9f90740c5f8f0ea07
Yo <franjomor@gmail.com>
2024-04-15 17:32:04 +0200

    feat: points logic

---
+++ src/app/src/services/gameServices.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const sendWord = (word, attemps) => {
+    if(attemps <= 6){
+        return axios
+                .post('apicheck', word)
+                .then(response => response.data)
+    }
+    else{
+        return {message: "too many attemps"}
+    }
+}
+const getPoints = (attemps) =>{
+    if(attemps < 1 || attemps > 6){
+        return {message: "attemps error"}
+    }
+    else{
+        return 100 - (15 * (attemps - 1))
+    }
+}
+export default { sendWord, getPoints }
