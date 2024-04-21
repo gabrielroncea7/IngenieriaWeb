0f2f1c444eef5fe0eeae838419d68197ff805d32
Yo <franjomor@gmail.com>
2024-04-21 10:56:17 +0200

    fix: word component

--- src/app/src/components/word/Word.js
+++ src/app/src/components/word/Word.js
@@ -5,23 +5,12 @@ import "./Word.css";
 
 function Word(props) {
 //gets the full word and size of word
-  const { word, size } = props;
-
-  // USES LETTERS TO RENDER FULL WORD
-  // uses 'split' to divide the word into an array of letters
-  // uses 'map' to iterate over the array of letters and make a Letter component for each letter of word
-  const renderLetters = () => {
-    return word.split('').map((letter, index) => (
-  // RENDERS LETTER BY LETTER
-      <Letter key={index} letter={letter} color={color} />
-    ));
-  };
+  const { word } = props;
 
   return (
-    <div>
-      <!-- // dix style= {{ fontSize: 'x px', fontWeight: 'bold' }} to make bigger letters -->
-      {renderLetters()}
-    </div>
+    <>
+      {word.map((letter, i) => <Letter letter={letter} key={'letter' + i}/>)}
+    </>
   );
 }
 
