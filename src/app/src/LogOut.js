974487c1a726a55d7d830a2f748c4e1c76cdd827
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-22 17:11:33 +0200

    fix: added header for responsive behavior

--- src/app/src/LogOut.js
+++ src/app/src/LogOut.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './components/header/Header';
 
 const LogOut = () => {
 //invalidates cookie to force logout -> makes cookie expire by setting an expiration date in the past and deleting token
