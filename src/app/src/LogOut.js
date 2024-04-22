568031263510e1afb8c06c803020baf57a860f8a
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-22 14:02:04 +0200

    feat: created frontend logout

---
+++ src/app/src/LogOut.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LogOut = () => {
+//invalidates cookie to force logout -> makes cookie expire by setting an expiration date in the past and deleting token
+document.cookie = 'sessionToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+
+  return (
+    <div>
+      <h2>Logged out succesfully</h2>
+      <p>Go back to <Link to="/signup">Main Page</Link> to Sign Up or Sign In</p>
+    </div>
+  );
+};
