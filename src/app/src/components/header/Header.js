ad3499f67ceefc40835450832ad1edcda7a38c3a
i12telol <118206793+i12telol@users.noreply.github.com>
2024-04-22 17:09:21 +0200

    fix: fixed comments

--- src/app/src/components/header/Header.js
+++ src/app/src/components/header/Header.js
@@ -3,15 +3,13 @@ import React from 'react';
 const Header = () => {
   
   return(
-    <!DOCTYPE html>
-      <html lang="en">
         <head>
           <meta charset="utf-8">
-    <!-- viewport allows responsive behavior, device-width depends on the user device, initial-scale sets the initial zoom to 1, user-scalable allows the user to zoom in and out-->
+    <!-- viewport allows responsive behavior, device-width depends on the user device, initial-scale sets the initial zoom to 1, user-scalable allows the user to zoom in and out -->
           <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
         </head>
-      </html>
   );
+  
 };
 
 export default Header;
