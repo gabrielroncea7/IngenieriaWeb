331072129bbf911d149b2e0fa9801fe75d5c8dc2
i12rumon <i12rumon@uco.es>
2024-04-25 11:09:38 +0200

    fix:  test userDAO modified

--- src/core/model/__tests__/userDAO.test.ts
+++ src/core/model/__tests__/userDAO.test.ts
@@ -1,6 +1,7 @@
 import UserDAO from '../userDAO/userDAO';
-
-
+import UserSchema from '../Schema/UserSchema';
+import mongoose from 'mongoose';
+import {User} from '../../domain/user/User'
 
 describe('UserDAO', () => {
   let userDAO: UserDAO;
@@ -9,58 +10,10 @@ describe('UserDAO', () => {
     userDAO = new UserDAO();
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  it('Probar funcion getUserbyname'  , async() =>{
+    const user  = await userDAO.getUserbyname('Plantilla');
+    expect(user).toEqual([]);
+  }
+  )
 
-  it('should get users from the database', async () => {
-    // Mock del método getUser para simular la respuesta de la base de datos
-    jest.spyOn(userDAO as any, 'ConnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'DisconnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'getUser').mockResolvedValueOnce(['user1', 'user2']);
-
-    // Ejecutar el método que se va a probar
-    const users = await userDAO.getUser();
-
-    // Verificar que se obtengan los usuarios esperados
-    expect(users).toEqual(['user1', 'user2']);
-  });
-
-  it('should set a new user in the database', async () => {
-    // Mock del método setUser para simular la inserción en la base de datos
-    jest.spyOn(userDAO as any, 'ConnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'DisconnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'setUser').mockResolvedValueOnce(1);
-
-    // Ejecutar el método que se va a probar
-    await userDAO.setUser('testUser', 'test@example.com', 'testPassword', 0, 0, 0);
-
-    // No hay ningún valor de retorno esperado, simplemente verificamos que no haya errores
-    // Si el método setUser no arroja ninguna excepción, asumimos que se ejecutó correctamente
-  });
-  it('should get user by name from the database', async () => {
-    // Mock del método getUserbyname para simular la respuesta de la base de datos
-    jest.spyOn(userDAO as any, 'ConnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'DisconnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'getUserbyname').mockResolvedValueOnce('user1');
-
-    // Ejecutar el método que se va a probar
-    const user = await userDAO.getUserbyname('testUser');
-
-    // Verificar que se obtenga el usuario esperado
-    expect(user).toEqual('user1');
-  });
-
-  it('should delete user from the database', async () => {
-    // Mock del método deleteUser para simular la eliminación en la base de datos
-    jest.spyOn(userDAO as any, 'ConnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'DisconnectDB').mockImplementationOnce(() => {});
-    jest.spyOn(userDAO as any, 'deleteUser').mockResolvedValueOnce(1);
-
-    // Ejecutar el método que se va a probar
-    await userDAO.deleteUser('userId');
-
-    // No hay ningún valor de retorno esperado, simplemente verificamos que no haya errores
-    // Si el método deleteUser no arroja ninguna excepción, asumimos que se ejecutó correctamente
-  });
 });
