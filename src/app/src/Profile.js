fdc8d59692eb69ce8d57ec50e2e07b9ea542c2c9
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-04 13:29:46 +0200

    feat: created Profile.js

---
+++ src/app/src/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import Header from './components/header/Header';
+import './index.css'
+
+const Profile = () => {
+
+
+  return {
+      <div>
+      	<Header />
+      	<h2>My Profile</h2>
+
+
+
+
+      	<p>Go back to Game? <Link to="/game">Play</Link></p>
+      </div>
+  );
+};
+  }
+}
+
+export default Profile;
