e5e669e47e354c3d27930dddce786f42fe80630a
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-06 16:15:14 +0200

    fix: changed delete profile message to display a pop up warning window

--- src/app/src/Profile.js
+++ src/app/src/Profile.js
@@ -3,9 +3,23 @@ import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import Cookies from 'js-cookie'; //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
 import Header from './components/header/Header';
+import Button from './components/button/Button';
 import './index.css'
 
+
 const Profile = () => {
+//controls the profile deletion pop up confirmation window
+  const [isOpen, setIsOpen] = useState(false);
+  const openDel = () => {
+    setIsOpen(true);
+  };
+  const closeDel = () => {
+    setIsOpen(false);
+  };
+
+
+
+  
   // sets variable username to be used as the found name in the cookie
   const [username, setUsername] = useState(''); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
 
@@ -21,17 +35,24 @@ const Profile = () => {
       <div>
       	<Header />
       	<h2>My Profile</h2>
-            <p>Your username is {username}</p> <!-- BORRAR CUANDO ESTÉ USABLE LA PROFILE API -->
+            <p>Your username is {username}</p> <!-- MODIFICAR CUANDO ESTÉ USABLE LA PROFILE API -->
 
   
             <!-- PONER ESTADISTICAS DE JUEGO -->
+
+
+
+
+
+
   
       	<div>
           <p><Link to="/game">Play Game</Link></p>
           <p><Link to="/logout">Log out</Link></p>
         </div>
         <div>
-          <p><Link to ="deleteprofile">Delete profile</Link></p>
+          <Button text="Delete Profile" onClick={openDel} />
+          <DeleteProfile isOpen={isOpen} onClose={closeDel} />
         </div>
       </div>
   );
