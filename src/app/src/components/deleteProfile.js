53ed3b44c18bc9d058bc87eff5df74064a0b4c10
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-06 16:24:36 +0200

    fix: added component for displaying warning before deleting profile

---
+++ src/app/src/components/deleteProfile.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./deleteProfile.css";
+import Button from '../button/Button';
+//CREATE POP UP WINDOW
+import Modal from 'react-modal';
+
+const DeleteProfile = ({ isOpen, onClose }) => {
+
+  //LLAMAR A LA API DE BORRAR PERFIL
+
+  
+  //returns text with warning about account deletion
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onClose}
+        ariaHideApp={false}
+      >
+
+        <h2 className='header'>Warning!</h2>
+        <div className='text'>
+          <p>
+            You are about to delete your profile!
+          </p>
+          <p>If you delete your profile, you won't be able to login using your credentials again, and all your data will be lost.</p>
+        </div>
+        <div>
+          <p>Click on the 'Delete Profile' button to delete your account forever.</p>
+        </div>
+        <Button text="Delete Profile" onClick={//ACCION DE BORRAR PERFIL} />
+        <div>
+          <p>If you don't want to delete your account, click on the 'Close Warning Window' button.</p>
+        </div>
+          <Button text="Close Warning Window" onClick={onClose} />
+      </Modal>
+    </>
+  );
+  
+};
+
+export default DeleteProfile;
