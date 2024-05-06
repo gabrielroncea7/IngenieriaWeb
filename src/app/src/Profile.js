495db6db38d3282a94377645cc4e3de52e4ba66f
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-06 13:04:44 +0200

    fix: added showing user's username

--- src/app/src/Profile.js
+++ src/app/src/Profile.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Cookies from 'js-cookie';
 import Header from './components/header/Header';
 import './index.css'
 
 const Profile = () => {
+  // sets variable username to be used as the found name in the cookie
+  const [username, setUsername] = useState('');
 
+  useEffect(() => {
+    // gets user from the global cookie to access the saved username
+    const user = Cookies.getJSON('user');
+    if (user && user.username) {
+      setUsername(user.username);
+    }
+  }, []);
 
   return {
       <div>
       	<Header />
       	<h2>My Profile</h2>
+            <p>Your username is {username}</p>
 
-
-
-
-      	<p>Go back to Game? <Link to="/game">Play</Link></p>
+  
+            <!-- PONER ESTADISTICAS DE JUEGO -->
+  
+      	<div>
+          <p><Link to="/game">Play Game</Link></p>
+          <p><Link to="/logout">Log out</Link></p>
+        </div>
       </div>
   );
 };
