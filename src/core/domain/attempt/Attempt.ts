1b3f42220a5b81c78db151f7184a8d9809383255
Yo <franjomor@gmail.com>
2024-05-12 16:50:52 +0200

    feat: attempt class

---
+++ src/core/domain/attempt/Attempt.ts
@@ -0,0 +1,108 @@
+import { Letter } from "../letter/Letter";
+
+interface IAttempt{
+    order: number,
+    word: [
+        {
+            value: String
+            color: String
+        }
+    ]
+}
+
+export class Attempt{
+    private _order: Number
+    private _word: Array<Letter>
+
+    constructor(
+        readonly order: number,
+        readonly word: Array<Letter>)
+    {
+        this._order = order
+        this._word = word
+    }
+    static jsonConstructor(
+        attempt: IAttempt): Attempt
+    {
+        let word:Array<Letter> = []
+
+        attempt.word.forEach(element => 
+            word.push(Letter.jsonConstructor(element))  
+        );
+
+        return new Attempt(attempt.order, word)
+    }
+    static create(
+        order: number,
+        word: Array<Letter>): Attempt | null
+    {
+        if(
+            Number.isInteger(order) &&
+            order >= 1 &&
+            order <= 6)
+        {
+            return new Attempt(order, word)
+        }
+        else{
+            return null
+        }
+    }
+    static jsonCreate(
+        attempt: IAttempt): Attempt | null
+    {
+        if(
+            Number.isInteger(attempt.order) &&
+            attempt.order >= 1 &&
+            attempt.order <= 6)
+        {
+            let word: Array<Letter> = []
+
+            let succeed: Boolean = true
+
+            for(let i = 0 ; i < attempt.word.length ; i++){
+                let letter: Letter | null = Letter.jsonCreate(attempt.word[i])
+
+                if(letter == null){
+                    succeed = false
+                    break
+                }
+                else{
+                    word.push(letter)
+                }
+            }
+
+            return succeed ? new Attempt(attempt.order, word) : null
+        }
+        else{
+            return null
+        }
+    }
+    getOrder(): Number{
+        return this._order
+    }
+    getWord(): Array<Letter>{
+        return this._word
+    }
+    setOrder(order: number): Boolean{
+        if(
+            Number.isInteger(order) &&
+            order >= 1 &&
+            order <= 6)
+        {
+            this._order = order
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    setWord(word: Array<Letter>){
+        this._word = word
+    }
+    toJson(): IAttempt{
+        return JSON.parse(
+            `{order: ${this._order}, word:[${
+                this._word.forEach(
+                    element => `${element.toJson()},`)}]}`)
+    }
+}
