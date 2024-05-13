157ed5b27662fadd41d87e37a02d737f0c3f3234
i12rumon <118832996+i12rumon@users.noreply.github.com>
2024-05-13 17:31:47 +0200

    fix: fixed gameDAO.test.ts

--- src/core/model/__tests__/gameDAO.test.ts
+++ src/core/model/__tests__/gameDAO.test.ts
@@ -2,37 +2,46 @@ import GameDAO from '../gameDAO/gameDAO';
 
 describe('Pruebas para la clase GameDAO', ()=>{
     const gameDAO1 = GameDAO.getInstance();
-    it('Probar función getLastGame', async () => {
-        let game = await gameDAO1.getLastGame('Prueba');
-        console.log(game);
-        if (game !== false) {
-            expect(typeof game).toBe('string');
-            expect(game.length).toBeGreaterThan(0);
-             
-        } else {
-            // Si game es false, la prueba debe fallar
-            fail('El juego no fue encontrado en la base de datos');
-            
-        }
-    });
+    const username = "Plantilla"
+    //Juego completo
+    const attempts = "[[{\"value\": \"H\", \"color\": \"green\"}, {\"value\": \"O\", \"color\": \"green\"}, {\"value\": \"L\", \"color\": \"green\"}, {\"value\": \"O\", \"color\":  \"red\"}]]"
+    //Un intento
+    const attempt = "[{\"value\": \"H\", \"color\": \"green\"}, {\"value\": \"O\", \"color\": \"green\"}, {\"value\": \"L\", \"color\": \"green\"}, {\"value\": \"A\", \"color\":  \"green\"}]"
+
+    
     
     it('Probar función addGame de un user que está en la BBDD', async () => {
-        let game = await gameDAO1.addGame('Prueba', '["3A"]');
-        expect(game).toEqual(true);
+        let game = await gameDAO1.addGame(username, attempts).then((game) =>{
+            expect(game).toEqual(true);
+        });
+
         
     });
 
 
 
     it('Probar función addAttempt', async () => {
-        // Agregar un juego primero
-        await gameDAO1.addGame('Prueba', '["3A"]');
-        // Agregar un intento al juego existente
-        let attemptAdded = await gameDAO1.addAttempt('Prueba', '[{"value": "4A", "color": "red"}]');
-        expect(attemptAdded).toEqual(true);
+        await gameDAO1.addAttempt(username, attempt).then((result) => {
+        expect(result).toEqual(true);
+        });
     });
 
+    it('Probar función getLastGame', async () => {
+        await gameDAO1.getLastGame('Plantilla').then((game) =>{
+            if (game !== false) {
+                expect(typeof game).toBe('string');
+                expect(game.length).toBeGreaterThan(0);
+                console.log(game);
+                 
+            } else {
+                // Si game es false, la prueba debe fallar
+                fail('El juego no fue encontrado en la base de datos');
+                
+            }
 
+        });
+        
+    });
 
 
 
