9c6780e03485e177c67bea7923a3cd2e35b34361
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-13 16:09:46 +0200

    fix: added link to profile api?

--- src/app/src/components/deleteProfile/DeleteProfile.js
+++ src/app/src/components/deleteProfile/DeleteProfile.js
@@ -3,11 +3,36 @@ import "./DeleteProfile.css";
 import Button from '../button/Button';
 //CREATE POP UP WINDOW
 import Modal from 'react-modal';
+//PROFILE API??
+import delete from '../services/profileServices';
 
 const DeleteProfile = ({ isOpen, onClose }) => {
 
-  //LLAMAR A LA API DE BORRAR PERFIL
+//sends signal to delete profile
+  const handleSubmit = async (event) => {
+    	const data = {username};
+    //supongo que la API se llamara profileServices, tipo accountServices y gameServices
+      const response = profileServices
+      .delete(username)
+      .then(res => res)
+      if(res.true){  
+        setMessage({
+          message: `The account has been deleted`,
+          color: 'green'
+        })
+      }
+    };
 
+
+  //pillar el username MIENTRAS PROFILE API NO ESTA FUNCIONAL
+  const [username, setUsername] = useState(''); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+  useEffect(() => { //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+    // gets user from the global cookie to access the saved username
+    const user = Cookies.getJSON('user'); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+    if (user && user.username) { //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+      setUsername(user.username); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+    } //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+  }, []); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
   
   //returns text with warning about account deletion
   return (
@@ -28,7 +53,7 @@ const DeleteProfile = ({ isOpen, onClose }) => {
         <div>
           <p>Click on the 'Delete Profile' button to delete your account forever.</p>
         </div>
-        <Button text="Delete Profile" onClick={/* ACCION DE BORRAR PERFIL */} />
+        <Button text="Delete Profile" onClick={handleSubmit} />
         <div>
           <p>If you don't want to delete your account, click on the 'Close Warning Window' button.</p>
         </div>
