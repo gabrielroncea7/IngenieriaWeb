b312ab0e371540e9aedf3045c0e28deccf021601
Yo <franjomor@gmail.com>
2024-05-13 17:23:40 +0200

    fix: error signin component

--- src/app/src/ErrorSignIn.js
+++ src/app/src/ErrorSignIn.js
@@ -3,13 +3,13 @@ import { useNavigate, Link } from 'react-router-dom';
 import Header from './components/header/Header';
 
 const ErrorSignIn = () => {
-    return {
+    return (
       <div>
           <Header />
           <h2>There was an error during the Sign In process</h2>
           <p><Link to="/App">Go back to main page</Link></p>
       </div>
-    }
+    )
 }
 
 export default ErrorSignIn;
