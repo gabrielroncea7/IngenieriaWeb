5ec5fb10d87918f3cf9e1a35d3b9c4eff371d1e7
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-14 09:45:28 +0200

    fixed: removed cookies

--- src/app/src/Profile.js
+++ src/app/src/Profile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { useCookie} from 'react-cookie'; //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
 import Header from './components/header/Header';
 import Button from './components/button/Button';
 import DeleteProfile from './components/deleteProfile/DeleteProfile';
@@ -29,17 +28,8 @@ const Profile = () => {
 
 
 
-  
-  // sets variable username to be used as the found name in the cookie
-  const [username, setUsername] = useState(''); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-
-  useEffect(() => { //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-    // gets user from the global cookie to access the saved username
-    const user = Cookies.getJSON('user'); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-    if (user && user.username) { //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-      setUsername(user.username); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-    } //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
-  }, []); //BORRAR CUANDO ESTÉ USABLE LA PROFILE API
+  // sets variable username to be used
+  const [username, setUsername] = useState('');
 
   return (
       <div>
