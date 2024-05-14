ac84bf6595b6bbe150f352fbac3be82fe5a487ac
Yo <franjomor@gmail.com>
2024-05-14 16:06:07 +0200

    fix: menu username

--- src/app/src/components/menu/Menu.jsx
+++ src/app/src/components/menu/Menu.jsx
@@ -1,11 +1,12 @@
 import InstructionsView from "../instructionView/InstructionView"
 import './menu.css'
 import UpperBarProfile from "../upperBarProfile/UpperBarProfile"
-const Menu = ({instructionsHandlers}) => {
+
+const Menu = ({instructionsHandlers, username}) => {
     return(
         <div className="menu">
             <InstructionsView handlers={instructionsHandlers}/>
-            
+            <UpperBarProfile username={username}/>
         </div>
     )
 }
