a56188ddad8d0b7e0528c87b0d468265b78fadc8
i12telol <118206793+i12telol@users.noreply.github.com>
2024-05-27 17:22:23 +0200

    fix: added restrictions to navigating, if session doesnt have username (user not logged) -> send user to signin

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
@@ -8,6 +7,8 @@ import Game from './Game';
 import Profile from './Profile';
 import ErrorSignIn from './ErrorSignIn';
 import ErrorSignUp from './ErrorSignUp';
+import Forbidden from './Forbidden';
+import sessionServices from './services/sessionServices';
 
 
 function App() {
@@ -19,11 +20,12 @@ function App() {
           <Route index element={<Navigate to="/signup" replace/>}/>
           <Route exact path="/signup" element={<SignUp/>} />
           <Route exact path="/signin" element={<SignIn/>} />
-          <Route exact path="/game" element={<Game/>} />
-          <Route exact path="/logout" element={<LogOut/>} />
-          <Route exact path="/profile" element={<Profile/>} />
-          <Route exact path="/errorsignin" element={<ErrorSignIn/>} />
-          <Route exact path="/errorsignup" element={<ErrorSignUp/>} />
+          <Route exact path="/forbidden" element={<Forbidden/>} />
+          {sessionServices.getUsername() ? <Route exact path="/game" element={<Game/>} /> : history.push(/forbidden) }
+          {sessionServices.getUsername() ? <Route exact path="/logout" element={<LogOut/>} /> : history.push(/forbidden) }
+          {sessionServices.getUsername() ? <Route exact path="/profile" element={<Profile/>} /> : history.push(/forbidden) }
+          {sessionServices.getUsername() ? <Route exact path="/errorsignin" element={<ErrorSignIn/>} /> : history.push(/forbidden) }
+          {sessionServices.getUsername() ? <Route exact path="/errorsignup" element={<ErrorSignUp/>} /> : history.push(/forbidden) }
         </Routes>
       </Router>
     </>
