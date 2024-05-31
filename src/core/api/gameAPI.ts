a7658f4329c0827253fa2c6f20ed85c483fc083f
gabrielroncea7 <gabrielroncea00@gmail.com>
2024-05-31 13:15:59 +0200

    fix: added interface, delete some functions useless

--- src/core/api/gameAPI.ts
+++ src/core/api/gameAPI.ts
@@ -5,12 +5,24 @@ const app = express();
 // Middleware para analizar los cuerpos JSON de las solicitudes
 app.use(express.json());
 
-// Endpoint para generar una nueva palabra para el juego
+
+interface ResponesGameAPI{
+    wordChecked: Array<{
+        letter: string,
+        color: string
+    }>,
+    win: Boolean,
+    points: number
+}
+
+
+// Devuelve el tamaño de la palabra
 app.get('/', (req: Request, res: Response) => {
     // Aquí puedes agregar lógica para generar una palabra para el juego
     const word = generateWord(); // Supongamos que esta función genera una palabra aleatoria
     res.status(200).json({ word });
 });
+
 
 // Endpoint para verificar una palabra ingresada por el jugador
 app.post('/', (req: Request, res: Response) => {
@@ -20,15 +32,10 @@ app.post('/', (req: Request, res: Response) => {
     res.status(200).json({ isValid });
 });
 
-// Función para generar una palabra para el juego (ejemplo)
-function generateWord(): string {
-    return 'ejemplo'; // Esto es solo un ejemplo, puedes implementar tu propia lógica para generar palabras
-}
 
-// Función para verificar una palabra ingresada por el jugador (ejemplo)
-function checkWord(word: string): boolean {
-    // Esto es solo un ejemplo, puedes implementar tu propia lógica para verificar palabras
-    return word === 'ejemplo';
-}
+
+
+
 
 export default app
+
