61e3449ce16cf226a87e49e98bfa9b56251d5909
i12telol <118206793+i12telol@users.noreply.github.com>
2024-06-02 22:19:33 +0200

    fix: hide change name

--- src/app/src/components/changeUsername/ChangeUsername.js
+++ src/app/src/components/changeUsername/ChangeUsername.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import "./ChangeUsername.css";
 import Button from '../button/Button';
 import Form from '../form/Form';
@@ -6,7 +6,7 @@ import Message from '../message/Message';
 //CREATE POP UP WINDOW
 import Modal from 'react-modal';
 //PROFILE API??
-import update from '../services/profileServices';
+//import update from '../services/profileServices';
 
 const ChangeUsername = ({ isOpen, onClose }) => {
 
@@ -15,28 +15,28 @@ const ChangeUsername = ({ isOpen, onClose }) => {
 
   const handleSubmit = async (event) => {
   //avoid empty form (no data to delete account)
-    	event.preventDefault();
+    //	event.preventDefault();
 
-      const user = profileServices
+      //const user = profileServices
       //id??
-      .get(id)
-      then(res => res)
+      //.get(id)
+      //then(res => res)
     //supongo que la API se llamara profileServices, tipo accountServices y gameServices
-      const response = profileServices
-      .update(user)
-      .then(res => res)
-      if(res.true){
-        setMessage({
-          message: `The account name has been changed`,
-          color: 'green'
-        })
-      }
-      else if(res.false){
-        setMessage({
-          message: 'There was an error and the account name is not changed',
-          color: 'red'
-        })
-      }
+      //const response = profileServices
+      //.update(user)
+      //.then(res => res)
+      //if(res.true){
+        //setMessage({
+          //message: `The account name has been changed`,
+          //color: 'green'
+        //})
+      //}
+      //else if(res.false){
+        //setMessage({
+          //message: 'There was an error and the account name is not changed',
+          //color: 'red'
+        //})
+      //}
     };
 
   const elements = [
@@ -64,7 +64,7 @@ const ChangeUsername = ({ isOpen, onClose }) => {
         <div>
           <Button text="Close Window" onClick={onClose} />
           </div>
-          <Message message={message.message} color={message.color} />
+          {/*<Message message={message.message} color={message.color} /> */}
       </Modal>
     </>
   );
