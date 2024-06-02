a69c3d02b4d8931273bdf93ae6abf604d0f5d3c0
i12telol <118206793+i12telol@users.noreply.github.com>
2024-06-02 22:20:22 +0200

    fix: now user icon works

--- src/app/src/components/upperBarProfile/UpperBarProfile.js
+++ src/app/src/components/upperBarProfile/UpperBarProfile.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 //imported user icon
-import { FaUser } from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import sessionServices from '../../services/sessionServices';
 
 const UpperBarProfile = () => {
 
-//LLAMAR A LA API DE SESION Y SACAR EL USERNAME
+//GET/SET USERNAME
+  const [username, setUsername] = useState('')
+  sessionServices.getUsername().then(name => setUsername(name))
   
-  // sets variable username to be used as the found name in the cookie
-  const [username, setUsername] = useState('');
   //take username and check if longer than 6 chars
-    setUsername(user.username);
     const str = {username}
     const len = str.length;
     if (len > 6) {
@@ -19,13 +20,15 @@ const UpperBarProfile = () => {
       const aux = '${user.username.substring(0,5)}...'
       setUsername(aux);
     }
+    
+    const icon = <FontAwesomeIcon icon={faCircleUser} />
   
-  return{
+  return(
     <div>
       <p><Link to="/logout">Sign Out</Link></p>
-      <p><Link to="/profile">{username} <FaUser /> </Link></p>
+      <p><Link to="/profile">{username} {icon} </Link></p>
     </div>
-  }
+  );
 }
 
 export default UpperBarProfile;
